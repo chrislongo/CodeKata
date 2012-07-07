5576de4a634fb94b6b5dc9f51aa7d42b8a3599ef
Chris Longo <chris.longo@gmail.com>
2012-07-07 14:22:33 -0400

    Iterative search in Scala

--- 01 - chop/src/main/scala/KataOneScala.scala
+++ 01 - chop/src/main/scala/KataOneScala.scala
@@ -6,7 +6,7 @@
 
 class KataOneScala
 {
-    def chop(find: Int, array: Array[Int]): Int = {
+    def recursiveChop(find: Int, array: Array[Int]): Int = {
         def recursive(start: Int, end: Int): Int = {
             if(start > end) -1 else {
                 val mid = (start + end) / 2
@@ -25,8 +25,30 @@ class KataOneScala
             case _ => recursive(0, array.size - 1)
         }
     }
+    
+    def iterativeChop(find: Int,  array: Array[Int]): Int = {
+        array match {
+            case null => -1
+            case _ => {
+                var start = 0
+                var end = array.size - 1
 
-    def chopFunctional(find: Int, array: Array[Int]): Int = {
+                while (start <= end) {
+                    val mid: Int = (start + end) / 2
+
+                    if (array(mid) > find)
+                        end = mid - 1
+                    else if (array(mid) < find)
+                        start = mid + 1
+                    else
+                        return mid
+                }
+            }
+        }
+        -1
+    }
+
+    def functionalChop(find: Int, array: Array[Int]): Int = {
         def recursive(start: Int, end: Int): Int = (start + end) / 2 match {
             case _ if start > end => -1
             case mid if array(mid) > find => recursive(start, mid - 1)
--- 01 - chop/src/test/java/KataOneScalaTest.scala
+++ 01 - chop/src/test/java/KataOneScalaTest.scala
@@ -12,50 +12,133 @@ class KataOneScalaTest extends FunSuite with ShouldMatchers {
 
     test("Found number 1 in array") {
         val array: Array[Int] = Array(1, 2, 3, 4, 5)
-        kata.chop(1, array) should equal(0)
+        kata.recursiveChop(1, array) should equal(0)
     }
 
     test("Found number 2 in array") {
         val array: Array[Int] = Array(1, 2, 3, 4, 5)
-        kata.chop(2, array) should equal(1)
+        kata.recursiveChop(2, array) should equal(1)
     }
 
     test("Found number 3 in array") {
         val array: Array[Int] = Array(1, 2, 3, 4, 5)
-        kata.chop(3, array) should equal(2)
+        kata.recursiveChop(3, array) should equal(2)
     }
 
     test("Found number 4 in array") {
         val array: Array[Int] = Array(1, 2, 3, 4, 5)
-        kata.chop(4, array) should equal(3)
+        kata.recursiveChop(4, array) should equal(3)
     }
 
     test("Found number 5 in array") {
         val array: Array[Int] = Array(1, 2, 3, 4, 5)
-        kata.chop(5, array) should equal(4)
+        kata.recursiveChop(5, array) should equal(4)
     }
 
     test("Number 0 not in array") {
         val array: Array[Int] = Array(1, 2, 3, 4, 5)
-        kata.chop(0, array) should equal(-1)
+        kata.recursiveChop(0, array) should equal(-1)
     }
 
     test("Number 6 not in array") {
         val array: Array[Int] = Array(1, 2, 3, 4, 5)
-        kata.chop(6, array) should equal(-1)
+        kata.recursiveChop(6, array) should equal(-1)
     }
 
     test("Null array returns -1") {
-        kata.chop(1, null) should equal(-1)
+        kata.recursiveChop(1, null) should equal(-1)
     }
 
     test("Zero length array returns -1") {
         val array: Array[Int] = Array()
-        kata.chop(1, array) should equal(-1)
+        kata.recursiveChop(1, array) should equal(-1)
     }
 
     test("Found number 1 in array (functional)") {
         val array: Array[Int] = Array(1, 2, 3, 4, 5)
-        kata.chopFunctional(1, array) should equal(0)
+        kata.functionalChop(1, array) should equal(0)
+    }
+
+    test("Found number 2 in array (functional)") {
+        val array: Array[Int] = Array(1, 2, 3, 4, 5)
+        kata.functionalChop(2, array) should equal(1)
+    }
+
+    test("Found number 3 in array (functional)") {
+        val array: Array[Int] = Array(1, 2, 3, 4, 5)
+        kata.functionalChop(3, array) should equal(2)
+    }
+
+    test("Found number 4 in array (functional)") {
+        val array: Array[Int] = Array(1, 2, 3, 4, 5)
+        kata.functionalChop(4, array) should equal(3)
+    }
+
+    test("Found number 5 in array (functional)") {
+        val array: Array[Int] = Array(1, 2, 3, 4, 5)
+        kata.functionalChop(5, array) should equal(4)
+    }
+
+    test("Number 0 not in array (functional)") {
+        val array: Array[Int] = Array(1, 2, 3, 4, 5)
+        kata.functionalChop(0, array) should equal(-1)
+    }
+
+    test("Number 6 not in array (functional)") {
+        val array: Array[Int] = Array(1, 2, 3, 4, 5)
+        kata.functionalChop(6, array) should equal(-1)
+    }
+
+    test("Null array returns -1 (functional)") {
+        kata.functionalChop(1, null) should equal(-1)
+    }
+
+    test("Zero length array returns -1 (functional)") {
+        val array: Array[Int] = Array()
+        kata.functionalChop(1, array) should equal(-1)
+    }
+
+    test("Found number 1 in array (iterartive)") {
+        val array: Array[Int] = Array(1, 2, 3, 4, 5)
+        kata.iterativeChop(1, array) should equal(0)
+    }
+
+    test("Found number 2 in array (iterartive)") {
+        val array: Array[Int] = Array(1, 2, 3, 4, 5)
+        kata.iterativeChop(2, array) should equal(1)
+    }
+
+    test("Found number 3 in array (iterartive)") {
+        val array: Array[Int] = Array(1, 2, 3, 4, 5)
+        kata.iterativeChop(3, array) should equal(2)
+    }
+
+    test("Found number 4 in array (iterartive)") {
+        val array: Array[Int] = Array(1, 2, 3, 4, 5)
+        kata.iterativeChop(4, array) should equal(3)
+    }
+
+    test("Found number 5 in array (iterartive)") {
+        val array: Array[Int] = Array(1, 2, 3, 4, 5)
+        kata.iterativeChop(5, array) should equal(4)
+    }
+
+    test("Number 0 not in array (iterartive)") {
+        val array: Array[Int] = Array(1, 2, 3, 4, 5)
+        kata.iterativeChop(0, array) should equal(-1)
+    }
+
+    test("Number 6 not in array (iterartive)") {
+        val array: Array[Int] = Array(1, 2, 3, 4, 5)
+        kata.iterativeChop(6, array) should equal(-1)
+    }
+
+    test("Null array returns -1 (iterartive)") {
+        kata.iterativeChop(1, null) should equal(-1)
+    }
+
+    test("Zero length array returns -1 (iterartive)") {
+        val array: Array[Int] = Array()
+        kata.iterativeChop(1, array) should equal(-1)
     }
 }
