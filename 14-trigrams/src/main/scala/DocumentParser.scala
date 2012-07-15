6371cf5fa75f622c36c16d9888ea1e271cdfc247
Chris Longo <chris.longo@gmail.com>
2012-07-15 17:05:00 -0400

    Kata Fourteen: Tom Swift Under Milk Wood

---
+++ 14-trigrams/src/main/scala/DocumentParser.scala
@@ -0,0 +1,60 @@
+import io.Source
+import util.Random
+
+/**
+ * User: chris
+ * Date: 7/15/12
+ * Time: 2:36 PM
+ */
+
+class DocumentParser
+{
+    val map = collection.mutable.Map.empty[String, collection.mutable.Set[String]]
+
+    def parse(fileName: String) {
+        val pattern = "\\w+".r
+        val text = Source.fromFile(fileName).mkString
+        val words = pattern.findAllIn(text).toArray
+
+        for(i <- 2 until words.size) {
+            var set = collection.mutable.Set.empty[String]
+            var key = (words(i - 2) + " " + words(i - 1))
+            key = key.toLowerCase
+
+            if(!map.contains(key))
+                map(key) = set
+            else
+                set = map(key)
+
+            set += words(i)
+        }
+    }
+
+    def printTrigram() {
+        val random = new Random()
+
+        var seed = randomSeed
+        var word = seed
+
+        for(i <- 0 until 100) {
+            print(word + " ")
+
+            if(i > 0 && i % 10 == 0)
+                println()
+
+            val set = map(seed)
+            val array = set.toArray
+            val index = random.nextInt(array.size)
+            val s = seed.split(" ")(1)
+
+            word = array(index)
+            seed = (s + " " + word).toLowerCase
+        }
+    }
+
+    def randomSeed: String = {
+       val random = new Random()
+       val array = map.keys.toArray
+       array(random.nextInt(array.size))
+    }
+}
---
+++ 14-trigrams/src/test/scala/DocumentParserTest.scala
@@ -0,0 +1,16 @@
+import org.scalatest.FunSuite
+
+/**
+ * User: chris
+ * Date: 7/15/12
+ * Time: 2:43 PM
+ */
+
+class DocumentParserTest extends FunSuite
+{
+    test("Parse document") {
+        val parser = new DocumentParser
+        parser.parse("data/mobydick.txt")
+        parser.printTrigram()
+    }
+}
